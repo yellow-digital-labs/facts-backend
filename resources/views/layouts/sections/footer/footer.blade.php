55b89285c76e75df3ef523a6e25cac5694faa5bd
Yellow Digital Labs <yellow.digital.labs@gmail.com>
2024-02-17 13:30:57 +0530

    initial theme setup
    initial theme setup

---
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -0,0 +1,15 @@
+<!-- Footer-->
+<footer class="content-footer footer bg-footer-theme">
+  <div class="{{ (!empty($containerNav) ? $containerNav : 'container-fluid') }}">
+    <div class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
+      <div>
+        © <script>
+          document.write(new Date().getFullYear())
+
+        </script>
+        , made with ❤️ by <a href="{{ (!empty(config('variables.creatorUrl')) ? config('variables.creatorUrl') : '') }}" target="_blank" class="fw-semibold">{{ (!empty(config('variables.creatorName')) ? config('variables.creatorName') : '') }}</a>
+      </div>
+    </div>
+  </div>
+</footer>
+<!--/ Footer-->
